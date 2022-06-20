5a0b291262308350a1f4f346b317235d66e48c10
Stepan Rudenko <s.rudenko584@gmail.com>
2022-06-20 11:38:27 +0300

    added locators with price, item name etc.

--- pages/locators.py
+++ pages/locators.py
@@ -6,4 +6,11 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    ITEM_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    ITEM_MESSAGE = (By.CSS_SELECTOR, ".alertinner strong")
+    ITEM_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    TOTAL_PRICE = (By.CSS_SELECTOR, ".alertinner p strong")
     
