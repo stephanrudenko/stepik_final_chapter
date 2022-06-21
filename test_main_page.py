d87acd6fa7a8c757842ffb51742cc6c1253ab6a5
Stepan Rudenko <s.rudenko584@gmail.com>
2022-06-21 12:45:40 +0300

    added tests with cart

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,9 @@
+import pytest
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
+@pytest.mark.skip(reason="works as expected")
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)                          # ініціалізуємо Page Object, передаємо в конструктор екземпляр драйверу та url адресу
@@ -9,28 +12,39 @@ def test_guest_can_go_to_login_page(browser):
     login_page = LoginPage(browser, browser.current_url)    # виконуємо метод сторінки - переходимо на сторінку логіну
     login_page.should_be_login_page()
 
+@pytest.mark.skip(reason="works as expected")
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.skip(reason="works as expected")
 def test_should_be_login_substring(browser):
     link = "http://selenium1py.pythonanywhere.com/accounts/login/"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_url()
 
+@pytest.mark.skip(reason="works as expected")
 def test_should_be_login_form(browser):
     link = "http://selenium1py.pythonanywhere.com/accounts/login/"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_form()
 
+@pytest.mark.skip(reason="works as expected")
 def test_should_be_register_form(browser):
     link = "http://selenium1py.pythonanywhere.com/accounts/login/"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_register_form()
 
-
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket()
+    basket_page.should_be_empty_basket_message()
