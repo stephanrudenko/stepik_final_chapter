7292309da370c8ace48465c6b8fbcb0e13735a95
Stepan Rudenko <s.rudenko584@gmail.com>
2022-06-21 19:20:32 +0300

    updated locators for LoginPage* class

--- pages/locators.py
+++ pages/locators.py
@@ -3,6 +3,10 @@ from selenium.webdriver.common.by import By
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    EMAIL_ADDRESS = (By.CSS_SELECTOR, "input#id_registration-email.form-control")
+    PASSWORD_FIELD = (By.CSS_SELECTOR, "input#id_registration-password1.form-control")
+    PASSWORD_FIELD_CONFIRM = (By.CSS_SELECTOR, "input#id_registration-password2.form-control")
+    REGISTER_USER = (By.CSS_SELECTOR, ".register_form button.btn.btn-lg.btn-primary")
 
 class ProductPageLocators():
     ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
@@ -13,7 +17,7 @@ class ProductPageLocators():
 
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class BasketPageLocators():
     BASKET_LINK = (By.CSS_SELECTOR, "span.btn-group a.btn.btn-default")
