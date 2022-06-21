2a8248c0d06506d0ff084bac87b7414b90009268
Stepan Rudenko <s.rudenko584@gmail.com>
2022-06-21 12:08:14 +0300

    added tests with cart

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 import pytest
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 
 @pytest.mark.skip(reason="promo ends")
 @pytest.mark.parametrize("promo_offer", [0, 1, 2, 3, 4, 5, 6,
@@ -39,18 +40,28 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.add_to_cart_button()
     page.success_message_disappeared()
 
+@pytest.mark.skip(reason="works as expected")   
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.skip(reason="works as expected")   
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
-
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/catalogue/the-girl-who-played-with-non-fire_203/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket()
+    basket_page.should_be_empty_basket_message()
+    
 
 
