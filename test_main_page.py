3aa40ef2f803e33871b00ce750ec1978e9f01d7c
Stepan Rudenko <s.rudenko584@gmail.com>
2022-06-21 19:41:52 +0300

    added login_guest mark and class with tests inside

--- test_main_page.py
+++ test_main_page.py
@@ -1,23 +1,24 @@
 import pytest
 from .pages.main_page import MainPage
+from .pages.base_page import BasePage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 
-@pytest.mark.skip(reason="works as expected")
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)                          # ініціалізуємо Page Object, передаємо в конструктор екземпляр драйверу та url адресу
-    page.open()                                             # відкриваємо сторінку
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)    # виконуємо метод сторінки - переходимо на сторінку логіну
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-@pytest.mark.skip(reason="works as expected")
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 @pytest.mark.skip(reason="works as expected")
 def test_should_be_login_substring(browser):
@@ -40,9 +41,10 @@ def test_should_be_register_form(browser):
     page.open()
     page.should_be_register_form()
 
+@pytest.mark.skip(reason="works as expected")
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "https://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = BasePage(browser, link)
     page.open()
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
